d59fd98e2cb6bcefda58a9c3d605064a5c470e8d
Vlad Efremov <vladefr97@yandex.ru>
2020-10-21 02:25:50 +0300

    Heroku production urls added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  restUrl: 'http://localhost:5000/api',
-  userUrl: 'http://localhost:5000/user'
+  restUrl: 'https://pizza-back-innoscripta.herokuapp.com/api',
+  userUrl: 'https://pizza-back-innoscripta.herokuapp.com/user'
 };
